44998aaf7ff04308c7230671fa5e77cebf2de2f4
Your Name <you@example.com>
2021-04-16 09:30:21 +0300

    stage3

--- Encrypt2.py
+++ Encrypt2.py
@@ -1,10 +1,23 @@
-import base64
-encoded_data = base64.b64encode("Encode this text")
+import sys
 
-print("Encoded text with base 64 is")
-print(encoded_data)
+def encrypt(text,s):
+    result = ""
+   # transverse the plain text
+    for i in range(len(text)):
+      char = text[i]
+      # Encrypt uppercase characters in plain text
+      
+      if (char.isupper()):
+         result += chr((ord(char) + s-65) % 26 + 65)
+      # Encrypt lowercase characters in plain text
+      else:
+         result += chr((ord(char) + s - 97) % 26 + 97)
+    return result
 
-decoded_data = base64.b64decode("RW5jb2RlIHRoaXMgdGV4dA==")
 
-print("decoded text is ")
-print(decoded_data)
+text = sys.argv[1]
+s = 4
+
+print ("Plain Text : " + text)
+print ("Shift pattern : " + str(s))
+print ("Cipher: " + encrypt(text,s))
--- Jenkinsfile
+++ Jenkinsfile
@@ -11,4 +11,5 @@ node{
     sh "env "
     echo params.text
     sh "echo 'Hello World'"
+    sh "python3 Encrypt.py"
 }
